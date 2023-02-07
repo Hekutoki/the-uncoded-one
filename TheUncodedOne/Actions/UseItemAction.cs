33ab0e154fe65a0c318e869fa86ec7509250b657
Hekutoki <igamm2@gmail.com>
2023-02-07 18:44:47 +0100

    feat: added Item and UseItemAction class

---
+++ TheUncodedOne/Actions/UseItemAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheUncodedOne.Characters;
+
+namespace TheUncodedOne.Actions;
+
+class UseItemAction : IAction
+{
+	public string Name => "Use item";
+
+	public void Perform(Character performingCharacter, Battle battle)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ TheUncodedOne/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheUncodedOne.Items;
+
+abstract class Item
+{
+	public string Name { get; }
+
+	public Item(string name) { Name = name; }
+
+	public abstract void Use();
+}
